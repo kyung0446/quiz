e854ebdb0303df1fc4c5955ad151dac258a2befc
kyung0446 <kyung0446@gmail.com>
2025-06-16 09:19:43 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import { useState } from "react";
 import quizData from "./data/quizData";
 import Categoris from "./Components/Categoris";
 import QuizPage from "./Components/QuizPage";
+import Results from "./Components/Results";
 
-const App = () => {  
+const App = () => {    
   const [category,setCategory] = useState("");
+  const [filterQuiz,setFilterQuiz] = useState([]);
+  const [finished, setFinished] = useState(false);
+  const [score,setScore] = useState(0);
   const onSelectCategory = (select)=>{
     setCategory(select);
+    
+    const quizArr = quizData.quizzes.filter((data)=>{ 
+      return data.category === select;
+    });
+    setFilterQuiz(quizArr);
+  }  
+  const handleFinish = (score) => {
+    setFinished(true);
+    setScore(score);
+  }
+  const handleRestart = ()=>{
+    setCategory('');
+    setFinished(false);
   }
   return (
     <div className="app">
-      <Categoris categoris={quizData.categories} 
-                 onSelect={onSelectCategory} 
-      />
-      <QuizPage />
+      { !category && !finished &&
+        <Categoris categoris={quizData.categories} 
+                  onSelect={onSelectCategory} 
+        />
+      }
+      {
+        category && !finished && (<QuizPage quiz={filterQuiz} onFinished={handleFinish} />)
+      }
+      {
+        finished && (<Results onRestart={handleRestart} appScore={score}/>)
+      }
     </div>
   );
 };
--- src/Components/QuizPage.js
+++ src/Components/QuizPage.js
@@ -1,8 +1,38 @@
+import { useState } from "react";
 
-const QuizPage = () => {
+const QuizPage = ({quiz,onFinished}) => {
+  const [currentIdx,setCurrentIdx] = useState(0);
+  const [score,setScore] = useState(0);
+  const currentQuiz = quiz[currentIdx];
+  const handleClick = (item)=>{
+    //정답인지 아닌지 체크
+    // if( item === currentQuiz.correct ){
+    //   setScore( score+10 );
+    // }
+    const result = (item===currentQuiz.correct) ? score+10:score;
+    
+    if( currentIdx+1 < quiz.length ){
+      //계속 실행이 되어야 하고
+      setCurrentIdx(currentIdx+1);
+      setScore(result);
+    } else {
+      //문제 끝
+      // console.log( "문제내는거 끝" );
+      onFinished(result);
+    }
+  }
   return (
     <div className="quiz-page">
-      <h2>퀴즈</h2>      
+      <h2>퀴즈({currentIdx+1}/{quiz.length})</h2>
+      <p style={{whiteSpace:"pre-line"}}>{currentQuiz.question}</p>
+      <div className="choices">
+        {
+          currentQuiz.choices.map((item,idx)=>{
+            return <button key={idx} onClick={()=>{handleClick(item)}}>{item}</button>;
+          })
+        }
+      </div>
+      <p>점수:{score}</p>
     </div>
   );
 };
